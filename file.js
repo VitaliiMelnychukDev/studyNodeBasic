debca37edeca774202b0f8708513d0f752aac80b
Vitalii <vitaliymelnychukdev@gmail.com>
2018-04-01 12:45:59 +0300

    stream

--- file.js
+++ file.js
@@ -3,12 +3,29 @@ let fs = require('fs');
 let fileText = fs.readFileSync(__dirname + "/files/test.txt", 'utf8');
 console.log(fileText);
 
-let fileTextSync = fs.readFile(__dirname + "/files/test.t1xt", 'utf8', function (err, data) {
+let fileTextSync = fs.readFile(__dirname + "/files/test.txt", 'utf8', function (err, data) {
     if(!err) {
         console.log(data);
     } else {
         console.log("FileReadingError: " + err.toString());
     }
+});
+
+let readableStream = fs.createReadStream(__dirname + "/files/stream.txt", {
+    encoding: 'utf8',
+    highWaterMark: 1024
+});
+
+let writableStream = fs.createWriteStream(__dirname + "/files/streamToWrite.txt");
+
+readableStream.on('data', function (data) {
+    console.log(data);
+    console.log(data.length);
+    writableStream.write(data);
+});
+
+readableStream.on('error', function (error) {
+    console.log(error);
 });
 
 console.log("End of code!");
