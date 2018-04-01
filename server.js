77bcee1c277ba0baede588c8cdae1baf91a2944f
Vitalii <vitaliymelnychukdev@gmail.com>
2018-04-01 14:14:58 +0300

    add html template to our server

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 let http = require('http');
+let fs = require('fs');
 
 let server = http.createServer(function (req, res) {
     res.writeHead(200, {
-        'Content-Type': 'text/plain',
+        'Content-Type': 'text/html',
     });
-    res.end('Hello!');
+
+    let html = fs.readFileSync(__dirname + '/templates/index.html', 'utf8');
+    let message = "Hello world!!!";
+    html = html.replace('{message}', message);
+    res.end(html);
 });
 
 server.listen(1060, '127.0.0.1');
