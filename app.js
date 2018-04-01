f66d72082d4a4cc0f8ff31f4ed2c9a05da05b369
Vitalii <vitaliymelnychukdev@gmail.com>
2018-04-01 17:29:09 +0300

    express middleware

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ let app = express();
 
 let port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function (req, res, next) {
+    console.log('Requested URL' + req.url);
+    next();
+});
+
 app.get('/', function (req, res) {
-    res.send("<html><head></head><body><h1>Hello</h1></body></html>");
+    res.send("<html><head><link href='assets/style.css' type='text/css' rel='stylesheet'/></head><body><h1>Hello</h1></body></html>");
 });
 
 app.get('/person/:id', function (req, res) {
