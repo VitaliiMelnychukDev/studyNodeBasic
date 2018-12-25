147dfe283faeba8c14ca99f039ca9e53b68c276a
VitaliiMelnychukDev <vitaliymelnychukdev@gmail.com>
2018-12-25 14:40:38 +0200

    Refactor buffer

--- nodeBasic/buffer.js
+++ nodeBasic/buffer.js
@@ -1,18 +1,17 @@
-
-let buff = new Buffer('Hello', 'utf8');
+const buff = Buffer.alloc(5, "Hello", "utf8");
 console.log(buff);
 console.log(buff.toString());
 console.log(buff.toJSON());
 console.log(buff[2]);
-
-buff.write('wo');
-console.log(buff);
+buff.write("repl", "utf8");
+console.log(buff.toString());
 
 
 //ES6 examples
-
 let es6Buffer = new ArrayBuffer(8);
+es6Buffer[0] = 't';
 let intBufferArr = new Int32Array(es6Buffer);
 intBufferArr[0] = 5;
 intBufferArr[1] = 23;
+console.log(es6Buffer);
 console.log(intBufferArr);
