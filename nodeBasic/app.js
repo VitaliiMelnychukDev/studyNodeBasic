c92d585f6ec4ccbcb1dcb21c0fe143bdf9495c4b
VitaliiMelnychukDev <vitaliymelnychukdev@gmail.com>
2018-12-25 15:58:17 +0200

    Move api-server to app

--- nodeBasic/app.js
+++ nodeBasic/app.js
@@ -1,10 +1,30 @@
+let http = require("http");
+let fs = require("fs");
+
+let server = http.createServer(function (req, res) {
+
+    if (req.url === "/") {
+        res.writeHead(200, {
+            "Content-Type": "text/html",
+        });
+        fs.createReadStream(__dirname + "/templates/hello.html").pipe(res);
+    } else if (req.url === "/api") {
+        res.writeHead(200, {
+            "Content-Type": "application/json",
+        });
+
+        let obj = {
+            message: "Hello"
+        };
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404, {
+            "Content-Type": "text/html",
+        });
+        fs.createReadStream(__dirname + "/templates/404.html").pipe(res);
+    }
 
 
-//module and require
-/*
-var greetSimple = require('./greetSingle');
-var greetModule = require('./greet');
+});
 
-greetSimple();
-greetModule.english();
-greetModule.spanish();*/
+server.listen(3000, "127.0.0.1");
