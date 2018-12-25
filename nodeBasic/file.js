1d886c927d72ac432800dd8171b99818e1d05b7b
VitaliiMelnychukDev <vitaliymelnychukdev@gmail.com>
2018-12-25 15:32:03 +0200

    Refactor streams

--- nodeBasic/file.js
+++ nodeBasic/file.js
@@ -1,41 +1,45 @@
-let fs = require('fs');
-let zlib = require('zlib');
+const fs = require("fs");
+const zlib = require("zlib");
 
-let fileText = fs.readFileSync(__dirname + "/files/test.txt", 'utf8');
-console.log(fileText);
+const FILES_FOLDER = __dirname + "/files/";
 
-let fileTextSync = fs.readFile(__dirname + "/files/test.txt", 'utf8', function (err, data) {
-    if (!err) {
-        console.log(data);
-    } else {
-        console.log("FileReadingError: " + err.toString());
-    }
-});
+const fileContent = fs.readFileSync(FILES_FOLDER + "test.txt", "utf8");
+console.log(fileContent);
 
-let readableStream = fs.createReadStream(__dirname + "/files/stream.txt", {
+function readFileAsync(pathToFile, encoding = "utf8") {
+    fs.readFile(pathToFile, encoding, function (err, data) {
+        if (!err) {
+            console.log("File content: " + data);
+        } else {
+            console.log("FileReadingError: " + err.toString());
+        }
+    });
+}
+
+readFileAsync(FILES_FOLDER + "test.txt");
+readFileAsync(FILES_FOLDER + "test1.txt");
+
+const readableStream = fs.createReadStream(FILES_FOLDER + "stream.txt", {
     encoding: 'utf8',
     highWaterMark: 1024
 });
 
+const writableStream = fs.createWriteStream(FILES_FOLDER + "streamToWrite.txt");
+const writableStreamPipe = fs.createWriteStream(FILES_FOLDER + "streamToWriteViaPipe.txt");
+const writableStreamGZ = fs.createWriteStream(FILES_FOLDER + "stream.txt.gz");
+const streamGz = zlib.createGzip();
 
-let writableStream = fs.createWriteStream(__dirname + "/files/streamToWrite.txt");
-
-let writableStreamGZ = fs.createWriteStream(__dirname + "/files/stream.txt.gz");
-
-let streamGz = zlib.createGzip();
-
-readableStream.pipe(writableStream);
-
+readableStream.pipe(writableStreamPipe);
+//Here readableStream  pipe to streamGZ. Inside of streamGZ data are compressed. And pass to writableStreamGZ
 readableStream.pipe(streamGz).pipe(writableStreamGZ);
 
-/*readableStream.on('data', function (data) {
+/*
+readableStream.on("data", function (data) {
     console.log(data);
     console.log(data.length);
     writableStream.write(data);
 });
 
-readableStream.on('error', function (error) {
+readableStream.on("error", function (error) {
     console.log(error);
 });*/
-
-console.log("End of code!");
